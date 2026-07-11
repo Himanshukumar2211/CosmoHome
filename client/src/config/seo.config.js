8e36f988d50f8e07f17feb16214f1c96a2fcd10b
Himanshu Kumar <kumarhimanshu6870@gmail.com>
2026-07-12 00:36:22 +0530

    Added my domain name

--- client/src/config/seo.config.js
+++ client/src/config/seo.config.js
@@ -1,5 +1,5 @@
 export const SEO_CONFIG = {
-  siteUrl: import.meta.env.VITE_SITE_URL || (import.meta.env.PROD ? 'https://MeraDomain' : 'http://localhost:5173'),
+  siteUrl: import.meta.env.VITE_SITE_URL || (import.meta.env.PROD ? 'https://cosmohome.co.in' : 'http://localhost:5173'),
   defaultTitle: 'Cosmo Home | Premium Home Salon Services',
   defaultDescription:
     'Cosmo Home brings premium salon, grooming, makeup, skincare, and beauty services to your doorstep with trained professionals.',
